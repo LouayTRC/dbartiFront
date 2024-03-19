8a5c14d682c340fc53a837bb9d7aa70210f11caf
louuu <louaytrc@gmail.com>
2024-03-19 17:25:02 +0100

    recipes & menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './components/user/recipes/recipes.component';
+import { MainComponent } from './components/user/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"main",component:MainComponent,children:[
+    {path:"recipes",component:RecipesComponent},
+    {path:"",redirectTo:"recipes",pathMatch:"full"}
+  ]},
+  {path:"",redirectTo:"main",pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './components/user/recipes/recipes.component';
+import { MainComponent } from './components/user/main/main.component';
+import { NavbarComponent } from './components/user/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
