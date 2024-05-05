79dd802d2430b4deee0dcb7c0382a393617ceaf4
louuu <louaytrc@gmail.com>
2024-05-05 14:34:55 +0100

    first commit

--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -2,5 +2,5 @@ import { Category } from "./category";
 import { Ingredient } from "./ingredient";
 
 export class Recipe {
-    constructor(public id:Number,public title:String,public description:String,public ingredients:Ingredient,public duration:Number,public pic:String,public nbCalories:Number,public category:Category,tuto?:String){}
+    constructor(public id:Number,public title:String,public description:String,public ingredients:Ingredient[],public duration:Number,public pic:String,public nbCalories:Number,public category:Category,tuto?:String){}
 }
